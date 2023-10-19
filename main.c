3eac26de0bd7c40b4d2da70bfb9458a9ce3ea32d
qksekf28 <hayoung902847@gmail.com>
2023-10-19 16:19:34 +0900

    5_nonstatic

--- main.c
+++ main.c
@@ -3,19 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int i;
+void sub(void);
+
 int main(void)
 {
-	for (i=0;i<5;i++)
-	{
-		f();
-		
-	}
-	return 0;
+   int i;
+   for (i=0; i<3; i++)
+   {
+      sub();
+}
+   return 0;
 }
 
-void f(void)
+void sub(void)
 {
-	for (i=0;i<10;i++)
-	printf("#");
+   int auto_count = 0;
+   int static_count = 0;
+   auto_count++;
+   static_count++;
+   printf("auto_count=%d\n", auto_count);
+   printf("static_count=%d\n", static_count);   
 }
